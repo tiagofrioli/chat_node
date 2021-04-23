ba358f59eca5ebc3d6e9e4aa232b5142f0e8d2f1
Tiago <tiago.frioli@gmail.com>
2021-04-22 23:59:16 -0300

    create migrations/entities/controller

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from 'express';
+import './database';
+import {routes} from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-	return response.send('Ola mundo !!');
-})
+app.use(express.json());
 
-
-app.post('/users', (request, response) => {
-	return response.json({ message: "Usuário salvo com sucesso" });
-});
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
